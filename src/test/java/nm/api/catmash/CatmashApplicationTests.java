b0f765cc14c6b38b2f245579b4f52f58495db5ca
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-06-20 17:15:25 +0200

    wip

--- src/test/java/nm/api/catmash/CatmashApplicationTests.java
+++ src/test/java/nm/api/catmash/CatmashApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import nm.api.catmash.Model.Cat;
@@ -20,7 +19,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@WebAppConfiguration
 class CatmashApplicationTests {
 
 	@Autowired
