185ed61511226a70d1d98833f5de2b1335e8a9f5
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-06-22 09:33:28 +0200

    wip configure units tests

--- src/test/java/nm/api/catmash/CatmashApplicationTests.java
+++ src/test/java/nm/api/catmash/CatmashApplicationTests.java
@@ -2,7 +2,7 @@ package nm.api.catmash;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest
 class CatmashApplicationTests {
 
 	@Autowired
